cfc3fb5946a9e381a68857890fe4cff7413457fd
gqq <gqq519@163.com>
2017-02-07 18:56:56 +0800

    项目创建，添加内存泄漏监测工具

---
+++ app/src/main/java/com/feicuiedu/eshop/EshopApplication.java
@@ -0,0 +1,29 @@
+package com.feicuiedu.eshop;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by gqq on 2017/2/7.
+ */
+
+public class EshopApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)){
+
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+
+            // 此进程是LeakCanary用于内存堆分析的.
+            // 不应该在此进程中做任何app初始化工作.
+            return;
+        }
+        LeakCanary.install(this);
+
+        // 项目正常初始化工作
+    }
+}
---
+++ app/src/main/java/com/feicuiedu/eshop/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.feicuiedu.eshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
